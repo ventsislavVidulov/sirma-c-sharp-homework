1314fc77920cfcc6e75e6e72e384cd8b8e4f96cd
ventsislavVidulov <xcen3k@gmail.com>
2024-11-29 19:28:11 +0200

    put the CLI in while cycle

--- AbstractionAndEncapsulation/Program.cs
+++ AbstractionAndEncapsulation/Program.cs
@@ -7,27 +7,36 @@ namespace AbstractionAndEncapsulation
         static void Main(string[] args)
         {
             int id = 0;
+
             List<InventoryItem> items = new List<InventoryItem>();
 
-            items.Add(new InventoryItem("Test Name", "Test category", 1.20, 10, "Test type", id++));
-            Console.WriteLine(items[0].Details);
-            Console.WriteLine(items[0].Id);
-            Console.WriteLine("Choose command from <add>, ");
+            //items.Add(new InventoryItem("Test Name", "Test category", 1.20, 10, "Test type", id++));
+            //Console.WriteLine(items[0].Details);
+            //Console.WriteLine(items[0].Id);
+            Console.WriteLine("Choose command from add, end");
             string command = Console.ReadLine();
 
-            if (command == "add") 
+            while (command != "end")
             {
-                Console.WriteLine("Enter item name");
-                string name = Console.ReadLine();
-                Console.WriteLine("Enter item category");
-                string details = Console.ReadLine();
-                Console.WriteLine("Enter item price");
-                double value = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine("Enter item quantity");
-                int quantity = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Enter item type");
-                string type = Console.ReadLine();
-                //name, category, price, quantity, and item type.
+                if (command == "add")
+                {
+                    Console.WriteLine("Enter item name");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Enter item category");
+                    string category = Console.ReadLine();
+                    Console.WriteLine("Enter item price");
+                    double price = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Enter item quantity");
+                    int quantity = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter item type between electronic, grocery, or fragile");
+                    string type = Console.ReadLine();
+                    //name, category, price, quantity, and item type.
+                    items.Add(new InventoryItem(name, category, price, quantity, type, id++));
+                    Console.WriteLine($"Successfully added {items.LastOrDefault().Details}");
+                }
+
+                Console.WriteLine("Choose command from add, end");
+                command = Console.ReadLine();
             }
         }
     }
